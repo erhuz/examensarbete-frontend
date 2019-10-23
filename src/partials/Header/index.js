5140adfe9dfdf943addc21e1c285fe290502e8cf
Benjamin Fransson <benjamin@cafcon.se>
2019-10-23 12:43:49 +0200

    Header partial consumes authReducer state

--- src/partials/Header/index.js
+++ src/partials/Header/index.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-react';
 import {
   Link
 } from "react-router-dom";
 import { ReactReduxContext } from 'react-redux';
+import { Menu, Dropdown, Icon } from 'semantic-ui-react';
 
 
 
 export default class Header extends Component {
-  state = {}
+  constructor(props){
+    super(props);
+
+    this.state = {
+      user: {
+        name: null,
+        email: null,
+        img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=120&q=80',
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.userLogin();
+  }
+
+  userLogin = () => {
+    const fakeForm = new FormData();
+    fakeForm.append('name', 'John Doe');
+    fakeForm.append('email', 'john.doe@example.com');
+    fakeForm.append('password', 'pass1234');
+    fakeForm.append('password_confirmation', 'pass1234');
+
+    fetch(process.env.REACT_APP_BACKEND_REST_API + '/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        ContentType: 'multipart/form-data',
+
+      },
+      body: fakeForm,
+    })
+    .then(res => res.json())
+    .then(json => {
+      this.props.SET_ACCESS_TOKEN(json.token);
+    })
+    .catch(err => {
+      console.error('Error Recieved: ' + err);
+
+    })
+  }
+
+  userLogout = () => {
+    this.props.REMOVE_ACCESS_TOKEN();
+  }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
@@ -40,11 +84,51 @@ export default class Header extends Component {
     return (
       <ReactReduxContext.Consumer>
         {({store}) => {
-          if(store.subscribe)
+          const { access_token } = store.getState().authReducer;
 
-          return<Menu>
+          let profileButton = (
+            <Menu.Item
+              position='right'
+              as={Link}
+              to='/login'
+              key='login'
+              active={activeItem === 'login'}
+              onClick={this.userLogin}
+              content={'Log In'}
+            />
+          )
+
+          if(access_token !== null){
+            console.log('access_token is not null');
+
+            profileButton = (
+              <Dropdown
+                as={Menu.Item}
+                position='right'
+                item
+                icon='user'
+                text='John Doe'
+                simple
+              >
+                <Dropdown.Menu>
+                  <Dropdown.Header>john.doe@example.com</Dropdown.Header>
+                  <Dropdown.Divider/>
+                  <Dropdown.Item onClick={this.userLogout}>Log Out</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            )
+          }else {
+            console.log('access_token is nulled');
+          }
+
+
+          return(
+          <Menu>
             { menuItems }
+
+            { profileButton }
           </Menu>
+          );
         }}
       </ReactReduxContext.Consumer>
     )
