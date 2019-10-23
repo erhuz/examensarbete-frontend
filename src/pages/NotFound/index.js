853e9b3d73f1a95134c9af797ecdd93551cf6d00
Benjamin Fransson <benjamin@cafcon.se>
2019-10-23 14:52:01 +0200

    Replaced about with support page

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from 'layouts/Default';
+
+function NotFound() {
+  return (
+    <Layout>
+      404 - Not Found
+    </Layout>
+  );
+}
+
+export default NotFound;
