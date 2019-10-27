c136838f90870ce0da03c33bd73a64ffae053309
Benjamin Fransson <benjamin@cafcon.se>
2019-10-27 12:15:04 +0100

    Added conditional routing & dashboard  page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -13,6 +13,7 @@ import Footer from 'partials/Footer';
 import Index from 'pages/Index';
 import Support from 'pages/Support';
 import Contact from 'pages/Contact';
+import Dashboard from 'pages/Dashboard';
 import NotFound from 'pages/NotFound';
 
 
@@ -33,16 +34,49 @@ class App extends Component {
       <Container style={appContainerStyles}>
         <Router>
           <Header />
-          <ReactReduxContext.Consumer>
-            {({store}) => {
-
-            }}
-          </ReactReduxContext.Consumer>
             <Switch>
+              {/* //TODO: Extract all routes into separate Routes Folder or File */}
               <Route exact path='/' component={Index} />
-              <Route path='/support' component={Support} />
               <Route path='/contact' component={Contact} />
-              <Route component={NotFound} />
+
+              {/* Authenticated Routes */}
+              <ReactReduxContext.Consumer>
+                {({store}) => {
+
+                  const { user } = store.getState().authReducer;
+                  const NoRouteFound = <Route component={NotFound} />
+                  let OptionalRoutes;
+
+                  // I dont know why, but it does what i want
+                  // Found out by accident
+                  if(user.roles !== null){
+                    const SupportRoute = <Route key='support' path='/support' component={Support} />;
+                    const DashboardRoute = <Route key='dashboard' path='/dashboard' component={Dashboard} />;
+
+                    OptionalRoutes = user.roles.map(role => {
+                      switch (role.name) {
+                        case 'employee':
+                          return DashboardRoute;
+
+                        case 'customer':
+                          return SupportRoute;
+
+                        default:
+                          return SupportRoute;
+                      }
+                    })
+
+                  }else{
+                    OptionalRoutes = NoRouteFound;
+                  }
+
+                  return (
+                    <>
+                      { OptionalRoutes }
+                    </>
+                  )
+                }}
+              </ReactReduxContext.Consumer>
             </Switch>
           <Footer/>
         </Router>
---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from 'layouts/Default';
+
+function Dashboard() {
+  return (
+    <Layout>
+      Dashboard
+    </Layout>
+  );
+}
+
+export default Dashboard;
