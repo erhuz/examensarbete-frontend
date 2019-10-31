37801c3f48beb6a5e84e214f99292eb67d6e5be5
Benjamin Fransson <benjamin@cafcon.se>
2019-10-31 12:30:44 +0100

    fixed broken function: userHasRole

--- src/helpers/user/index.js
+++ src/helpers/user/index.js
@@ -1,13 +1,11 @@
-export const userHasRole = (roleName, user) => {
-  // If role found
-  user.roles.forEach(role => {
-    if(role.name === roleName){
-      return true;
-    }
-  });
+export const userHasRole = (user, roleName) => {
+  const roles = getUserRoles(user);
 
-  // If role not found
-  return false;
+  if(roles.includes(roleName)){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 export const getUserRoles = (user) => {
