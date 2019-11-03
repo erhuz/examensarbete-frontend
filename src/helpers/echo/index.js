fcfe03d1111b41cc1b0b4691166c3bff01f9cfec
Benjamin Fransson <benjamin@cafcon.se>
2019-11-03 21:28:41 +0100

    Modified echo helper to send authorization token

--- src/helpers/echo/index.js
+++ src/helpers/echo/index.js
@@ -4,19 +4,24 @@ import Echo from 'laravel-echo';
 // Set up Pusher for Echo
 // window.Pusher = Pusher;
 
-// Set up Laravel Echo
-const optionsForEcho = {
-  broadcaster: 'pusher',
-  key: process.env.REACT_APP_PUSHER_KEY,
-  cluster: 'eu',
-  forceTLS: true,
-  encrypted: false,
-  authEndpoint: 'http://vcs-backend.test/broadcasting/auth',
-  auth: {
-    headers: {
-      Accept: 'Application/json',
+const echoHelper = (access_token) => {
+  // Set up Laravel Echo
+  const optionsForEcho = {
+    broadcaster: 'pusher',
+    key: process.env.REACT_APP_PUSHER_KEY,
+    cluster: 'eu',
+    forceTLS: true,
+    encrypted: false,
+    authEndpoint: 'http://vcs-backend.test/broadcasting/auth',
+    auth: {
+      headers: {
+        Accept: 'Application/json',
+        Authorization: `Bearer ${access_token}`,
+      }
     }
   }
+
+  return new Echo(optionsForEcho);
 }
 
-export default new Echo(optionsForEcho);
+export default echoHelper;
