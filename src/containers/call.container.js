92b8757bf0ffb278740ec5d15689e470bf565c7b
Benjamin Fransson <benjamin@cafcon.se>
2019-11-04 20:14:17 +0100

    Created call redux container

---
+++ src/containers/call.container.js
@@ -0,0 +1,32 @@
+import { connect } from 'react-redux';
+import Call from 'components/Call';
+import {
+  SET_CURRENT_CALL,
+  UPDATE_CURRENT_CALL,
+  REMOVE_CURRENT_CALL,
+} from 'actions/call.actions';
+
+const mapStateToProps = ({callReducer, authReducer}) => {
+  return {callReducer, authReducer};
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    SET_CURRENT_CALL: (token) => {
+      dispatch(SET_CURRENT_CALL(token));
+    },
+    UPDATE_CURRENT_CALL: (token) => {
+      dispatch(UPDATE_CURRENT_CALL(token));
+    },
+    REMOVE_CURRENT_CALL: () => {
+      dispatch(REMOVE_CURRENT_CALL());
+    },
+  }
+}
+
+const CallContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Call)
+
+export default CallContainer;
