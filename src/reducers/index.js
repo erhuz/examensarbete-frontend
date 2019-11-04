74a994c5ed9dc9779dabba813abc875b4f8455aa
Benjamin Fransson <benjamin@cafcon.se>
2019-11-04 19:12:06 +0100

    implemented call reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 // Import all reducers
 import authReducer from "reducers/auth.reducer";
+import callReducer from "reducers/call.reducer";
 
 // Export combined reducer
 const combinedReducer = combineReducers({
   authReducer,
+  callReducer,
 })
 
 export default combinedReducer;
