4009476db170501d4b19bce231d98aea6098f082
Benjamin Fransson <benjamin@cafcon.se>
2019-11-04 18:32:53 +0100

    Added LoginForm to homepage

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from 'layouts/Default';
+import LoginForm from 'containers/login.container';
 
 function Index() {
   return (
     <Layout>
       Home
+      <LoginForm/>
     </Layout>
   );
 }
