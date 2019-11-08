07957980a7cd9c3d674a682b77381c541c05f22e
Benjamin Fransson <benjamin@cafcon.se>
2019-11-08 14:58:49 +0100

    Removed ...state due to not needed

--- src/reducers/call.reducer.js
+++ src/reducers/call.reducer.js
@@ -7,19 +7,16 @@ const callReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_CURRENT_CALL':
       return {
-        ...state,
         call: action.payload
       }
 
     case 'UPDATE_CURRENT_CALL':
       return {
-        ...state,
         call: action.payload
       }
 
     case 'REMOVE_CURRENT_CALL':
       return {
-        ...state,
         call: null
       }
 
